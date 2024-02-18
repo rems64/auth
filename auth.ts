aa84c1d7a4f6f162f6b27f1555bad225319a77a9
Remy Fayet <rems64des@gmail.com>
2024-02-18 18:47:46 +0100

    Base commit

---
+++ auth.ts
@@ -0,0 +1,35 @@
+import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { getUserById } from "./data/user";
+import { db } from "@/lib/db";
+import authConfig from "@/auth.config";
+import { UserRole } from "@prisma/client";
+
+export const {
+    handlers: { GET, POST },
+    auth,
+    signIn,
+    signOut,
+} = NextAuth({
+    callbacks: {
+        async session({ token, session }) {
+            if (token.sub && session.user) {
+                session.user.role = token.role as UserRole;
+            }
+            return session;
+        },
+        async jwt({ token }) {
+            
+            if (!token.sub) return token;
+            
+            const existingUser = await getUserById(token.sub);
+            
+            if (!existingUser) return token;
+            token.role = existingUser.role;
+            return token;
+        }
+    },
+    adapter: PrismaAdapter(db),
+    session: { strategy: "jwt" },
+    ...authConfig
+})
---
+++ components/auth/header.tsx
@@ -0,0 +1,38 @@
+import { Poppins } from "next/font/google";
+
+import { cn } from "@/lib/utils";
+
+import { FaLock } from "react-icons/fa";
+
+const font = Poppins({
+    subsets: ["latin"],
+    weight: ["600"],
+});
+
+interface HeaderProps {
+    label: string;
+}
+
+
+export const Header = ({
+    label,
+}: HeaderProps) => {
+    return (
+        <div className="w-full flex flex-col gap-y-4 items-center justify-center">
+            <h1 className={cn(
+                "text-3xl font-semibold",
+                font.className
+            )}>
+                <div className="flex space-x-2">
+                    <FaLock />
+                    <p>
+                        Auth
+                    </p>
+                </div>
+            </h1>
+            <p className="text-muted-foreground text-sm">
+                {label}
+            </p>
+        </div>
+    )
+}
